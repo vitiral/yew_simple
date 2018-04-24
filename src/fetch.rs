b18f89e07068f1825e555fe1276943d73901c9f0
Garrett Berg <vitiral@gmail.com>
2018-04-24 08:23:55 -0600

    not working

--- src/fetch.rs
+++ src/fetch.rs
@@ -111,7 +111,7 @@ impl FetchService {
         // Prepare the response callback.
         // Notice that the callback signature must match the call from the javascript
         // side. There is no static check at this point.
-        let js_callback = move |success: bool, response: Value, recv_body: String| {
+        let js_callback = move |success: bool, response: Value, recv_body: String| -> () {
             let mut response_builder = Response::builder();
 
             // Deserialize response status.
@@ -141,142 +141,38 @@ impl FetchService {
             yew_callback.emit(response);
         };
 
-        unimplemented!();
 
-
-
-    //     let handle = js! {
-    //         var data = {
-    //             method: @{parts.method.as_str()},
-    //             body: @{body.into()},
-    //             headers: @{header_map},
-    //         };
-    //         var request = new Request(@{uri}, data);
-    //         var callback = @{callback};
-    //         var handle = {
-    //             active: true,
-    //             callback,
-    //         };
-    //         fetch(request).then(function(response) {
-    //             response.text().then(function(data) {
-    //                 if (handle.active == true) {
-    //                     handle.active = false;
-    //                     callback(true, response, data);
-    //                     callback.drop();
-    //                 }
-    //             }).catch(function(err) {
-    //                 if (handle.active == true) {
-    //                     handle.active = false;
-    //                     callback(false, response, data);
-    //                     callback.drop();
-    //                 }
-    //             });
-    //         });
-    //         return handle;
-    //     };
-    //     FetchTask(Some(handle))
+        let handle = js! {
+            var data = {
+                method: @{parts.method.as_str()},
+                body: @{body.into()},
+                headers: @{header_map},
+            };
+            var request = new Request(@{uri}, data);
+            var callback = @{js_callback};
+            var handle = {
+                active: true,
+                callback,
+            };
+            fetch(request).then(function(response) {
+                response.text().then(function(data) {
+                    if (handle.active == true) {
+                        handle.active = false;
+                        callback(true, response, data);
+                        callback.drop();
+                    }
+                }).catch(function(err) {
+                    if (handle.active == true) {
+                        handle.active = false;
+                        callback(false, response, data);
+                        callback.drop();
+                    }
+                });
+            });
+            return handle;
+        };
+        FetchTask(Some(handle))
     }
-
-    // pub fn fetch<IN, OUT: 'static>(
-    //     &mut self,
-    //     request: Request<IN>,
-    //     callback: Callback<Response<OUT>>,
-    // ) -> FetchTask
-    // where
-    //     IN: Into<Storable>,
-    //     OUT: From<Restorable>,
-    // {
-    //     // Consume request as parts and body.
-    //     let (parts, body) = request.into_parts();
-
-    //     // Map headers into a Js serializable HashMap.
-    //     let header_map: HashMap<&str, &str> = parts
-    //         .headers
-    //         .iter()
-    //         .map(|(k, v)| {
-    //             (
-    //                 k.as_str(),
-    //                 v.to_str().expect(
-    //                     format!("Unparsable request header {}: {:?}", k.as_str(), v).as_str(),
-    //                 ),
-    //             )
-    //         })
-    //         .collect();
-
-    //     // Formats URI.
-    //     let uri = format!("{}", parts.uri);
-
-    //     // Prepare the response callback.
-    //     // Notice that the callback signature must match the call from the javascript
-    //     // side. There is no static check at this point.
-    //     let callback = move |success: bool, response: Value, body: String| {
-    //         let mut response_builder = Response::builder();
-
-    //         // Deserialize response status.
-    //         let status = u16::try_from(js!{
-    //             return @{&response}.status;
-    //         });
-
-    //         if let Ok(code) = status {
-    //             response_builder.status(code);
-    //         }
-
-    //         // Deserialize response headers.
-    //         let headers: HashMap<String, String> = HashMap::try_from(js!{
-    //             var map = {};
-    //             @{&response}.headers.forEach(function(value, key) {
-    //                 map[key] = value;
-    //             });
-    //             return map;
-    //         }).unwrap_or_default();
-
-    //         for (key, values) in &headers {
-    //             response_builder.header(key.as_str(), values.as_str());
-    //         }
-
-    //         // Deserialize and wrap response body into a Restorable object.
-    //         let data = if success {
-    //             Ok(body)
-    //         } else {
-    //             // Err(FetchError::FailedResponse.into())
-    //             unimplemented!()
-    //         };
-    //         let out = OUT::from(data);
-    //         let response = response_builder.body(out).unwrap();
-    //         callback.emit(response);
-    //     };
-
-    //     let handle = js! {
-    //         var data = {
-    //             method: @{parts.method.as_str()},
-    //             body: @{body.into()},
-    //             headers: @{header_map},
-    //         };
-    //         var request = new Request(@{uri}, data);
-    //         var callback = @{callback};
-    //         var handle = {
-    //             active: true,
-    //             callback,
-    //         };
-    //         fetch(request).then(function(response) {
-    //             response.text().then(function(data) {
-    //                 if (handle.active == true) {
-    //                     handle.active = false;
-    //                     callback(true, response, data);
-    //                     callback.drop();
-    //                 }
-    //             }).catch(function(err) {
-    //                 if (handle.active == true) {
-    //                     handle.active = false;
-    //                     callback(false, response, data);
-    //                     callback.drop();
-    //                 }
-    //             });
-    //         });
-    //         return handle;
-    //     };
-    //     FetchTask(Some(handle))
-    // }
 }
 
 impl Task for FetchTask {
