69eb2c53a84b2196feb5347059c181360762f675
Garrett Berg <vitiral@gmail.com>
2018-04-24 08:09:37 -0600

    fetch kind of compiles

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 //! This module contains simple services and tasks for the [`yew`] framework.
 //!
 //! [`yew`]: https://github.com/DenisKolodin/yew
+#![recursion_limit="256"]
 
 extern crate yew;
 #[macro_use] extern crate stdweb;
 pub extern crate url;
+pub extern crate http;
 
 mod router;
+mod fetch;
 
 pub use router::{RouterTask, RouteInfo};
